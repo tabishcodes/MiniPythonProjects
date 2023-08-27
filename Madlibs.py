b4c4f2f129835e4e35b43e209e5ea8ac1b50a5be
TabishCodes <tabishsheikhcs@gmail.com>
2023-08-27 14:03:50 +0530

    August27

---
+++ Madlibs.py
@@ -0,0 +1,24 @@
+adj = input("A light: ")
+adj1 = input("A place: ")
+adj2 = input("A food: ")
+adj3 = input("A bird: ")
+adj4 = input("A color: ")
+adj5 = input("A car: ")
+adj6 = input("A bike: ")
+adj7 = input("A toy: ")
+adj8 = input("A game: ")
+adj8 = input("A sport: ")
+
+madlib = f"The day had begun on a {adj} note. The sun finally \
+peeked through the rain for the first time in a week, and the \
+birds were {adj1} in its warmth. There was no way to anticipate \
+what was about to happen. It was a worst-case {adj2} and there \
+was no way out of it.\
+If you can imagine a furry {adj3} seven feet tall, with the face \
+of an intelligent {adj4} and the braincase of a man, you'll have a \
+rough idea of what they looked like -- {adj5} for their teeth. \
+The canines would have fitted better in the face of a {adj6}, and \
+showed at the corners of {adj7} wide, thin-lipped mouths, giving {adj8} \
+an expression of ferocity."
+
+print(madlib)
