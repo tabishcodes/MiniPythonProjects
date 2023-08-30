388eb0d964c061b4784a25c07d46ccf039efcda0
TabishCodes <tabishsheikhcs@gmail.com>
2023-08-30 18:58:01 +0530

    August30

---
+++ Hangman.py
@@ -0,0 +1,16 @@
+import random
+from words import words
+import string
+
+def get_valid_word(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+
+    return word.upper()
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
